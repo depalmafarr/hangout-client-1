7bf736fea1092f4d333906af359ea163ead1d8a5
depalmafarr <brandondepalmafarr@gmail.com>
2020-04-21 16:29:08 -0400

    Created auth events
    Created sign in, sign up, sign out, and changePassword.
    DID NOT create any html buttons but tested sign up and sign in with CURLS
    Close #1

---
+++ curl-scripts/auth/sign-in.sh
@@ -0,0 +1,11 @@
+curl "https://shrouded-refuge-81940.herokuapp.com/sign-in" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'"
+    }
+  }'
+echo
