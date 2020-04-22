849e202915e0315cba6be4b1bcc025cd6b2ffc1e
Timeonlytells@gmail.com <Timeonlytells@gmail.com>
2020-04-22 09:51:35 -0400

    Addded hangs initial scripts Began coding events javascript to link from app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,7 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const authEvents = require('./auth/events.js')
+const hangEvents = require('./hangs/events.js')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
@@ -13,4 +14,12 @@ $(() => {
   $('#sign-in').on('submit', authEvents.onSignIn)
   $('#change-password').on('submit', authEvents.onChangePassword)
   $('#sign-out').on('submit', authEvents.onSignOut)
+
+
+
+// for showing all hangs
+  $('#').on('click', hangEvents.onShowHangs)
+
+  // for creating new hangs
+$('#').on('submit', hangEvents.onNewHang)
 })
