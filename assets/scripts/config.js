50c739103b3f81843188d941867abd20c4a7d85e
Danpowers24 <danpowers610@gmail.com>
2020-04-23 22:19:28 -0400

    Add heroku link to production api URL
    Co-authored-by: Trey Hilson, Brandon DePalmaFarr, Daniel Powers, Ross Northrup

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://shrouded-refuge-81940.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
